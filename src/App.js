7a6e491a5c18e01cf77c3bca3b9db5876f4b7e17
Nikolai Limbrunner <ntli@kth.se>
2022-04-14 17:08:31 +0200

    Static text for React App

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            { DevOps rocks! }
+            DevOps rocks!
         </p>
       </header>
     </div>
