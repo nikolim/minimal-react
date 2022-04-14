ed02c8e28624b7c7aabd805a6bfbecd5d31644d6
Nikolai Limbrunner <ntli@kth.se>
2022-04-14 16:42:12 +0200

    Remove fetch from node

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ function App() {
   const REST_API_URL = 'http://173.18.0.3:4000/data';
   const [data, setData] = useState(" ");
 
-  fetch(REST_API_URL, {
-      method: 'GET',
-      mode: 'no-cors',
-    }).then(res => {
-      return res.json()
-    }).then(res => setData(res));
+  // fetch(REST_API_URL, {
+  //     method: 'GET',
+  //     mode: 'no-cors',
+  //   }).then(res => {
+  //     return res.json()
+  //   }).then(res => setData(res));
   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            { data }
+            { DevOps rocks! }
         </p>
       </header>
     </div>
